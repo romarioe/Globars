7039c27ed9be8ed5217aa86d494e346887a34468
Roman Efimov <romarioe@mail.ru>
2020-09-30 12:21:06 +0300

    Initial Commit

---
+++ Globars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Globars
+//
+//  Created by Roman Efimov on 30.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
